9909049520dab8e05f40d0e0dfe0cf0baa733b98
Ioan Buraga <56740358+BuragaIonut@users.noreply.github.com>
2021-04-03 21:40:54 +0300

    #adding some comments

--- GUI.py
+++ GUI.py
@@ -7,25 +7,23 @@ def return_strings():
     return e1, e2
 
 
-canvas1 = tk.Canvas(master, width=300, height=120)
+canvas1 = tk.Canvas(master, width=400, height=120)
 canvas1.pack()
 
-label1 = tk.Label(master, text="Location:", font='Calibri 12 bold')
-canvas1.create_window(70, 20, window=label1)
-label2 = tk.Label(master, text="Date:", font='Calibri 12 bold')
-canvas1.create_window(70, 50, window=label2)
+label1 = tk.Label(master, text="Location: (ex: Cluj Iulius Mall)", font='Calibri 12 bold')
+canvas1.create_window(120, 20, window=label1)
+label2 = tk.Label(master, text="Date: (ex: 22/12/2020)", font='Calibri 12 bold')
+canvas1.create_window(120, 50, window=label2)
 
 e1 = tk.Entry(master)
-canvas1.create_window(180, 20, window=e1)
+canvas1.create_window(300, 20, window=e1)
 e2 = tk.Entry(master)
-canvas1.create_window(180, 50, window=e2)
-
+canvas1.create_window(300, 50, window=e2)
 
 file = tk.Button(master,
-          text='SELECT SCHEDULE',
-          command=lambda:[return_strings,master.quit()],
+                 text='SELECT SCHEDULE',
+                 command=lambda: [return_strings, master.quit()],
                  font='Calibri 12 bold')
 
-canvas1.create_window(150, 100, window=file)
+canvas1.create_window(200, 100, window=file)
 tk.mainloop()
-
